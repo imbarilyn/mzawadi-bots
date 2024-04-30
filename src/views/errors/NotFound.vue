3deb71963df602a6aa2b84682da45c484ae02a4e
muhonja <linahmuhonjaimbari>
2024-04-30 16:03:24 +0300

    remove .ts extension

--- src/views/errors/NotFound.vue
+++ src/views/errors/NotFound.vue
@@ -1,22 +1,25 @@
 <script lang="ts" setup>
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import { type Tab, useTabsStore } from '../../stores/admin/tabs'
 
-import {useRouter} from "vue-router";
-import {ref} from "vue";
-import {Tab, useTabsStore} from "../../stores/admin/tabs.ts";
+const router = useRouter()
+const tabsStore = useTabsStore()
 
-const router = useRouter();
-const tabsStore = useTabsStore();
-
-const homeTab = ref();
-homeTab.value = tabsStore.getTabs.find((tab: Tab) => tab.name === "Home");
+const homeTab = ref()
+homeTab.value = tabsStore.getTabs.find((tab: Tab) => tab.name === 'Home')
 const onGoBackHomeClick = async () => {
-  await router.push({name: "AdminHome", params: {page: homeTab.value.to}, query: {pageId: homeTab.value.pageId}});
-};
+  await router.push({
+    name: 'AdminHome',
+    params: { page: homeTab.value.to },
+    query: { pageId: homeTab.value.pageId }
+  })
+}
 </script>
 
 <template>
   <div class="flex-1 flex flex-col justify-center items-center h-screen">
-    <img alt="404 error" class="w-1/4" src="../../assets/imgs/404-error.png"/>
+    <img alt="404 error" class="w-1/4" src="../../assets/imgs/404-error.png" />
 
     <div class="text-center">
       <h1 class="text-3xl font-bold text-neutral-900">Page not found</h1>
@@ -25,14 +28,13 @@ const onGoBackHomeClick = async () => {
 
     <div class="mt-4">
       <button
-          class="btn btn-primary btn-sm md:btn-md lg:btn-lg xl:btn-xl normal-case"
-          @click="onGoBackHomeClick">
+        class="btn btn-primary btn-sm md:btn-md lg:btn-lg xl:btn-xl normal-case"
+        @click="onGoBackHomeClick"
+      >
         Go back home
       </button>
     </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
