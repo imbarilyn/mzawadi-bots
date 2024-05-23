2ffb82aa731890f41fad1602a51f2b5b07fda1f1
muhonja <linahmuhonjaimbari>
2024-05-23 09:25:19 +0300

    add route to create chatbot

--- src/components/Admin/TopBar.vue
+++ src/components/Admin/TopBar.vue
@@ -1,35 +1,42 @@
 <script lang="ts" setup>
-import {type Tab, useTabsStore} from "../..//stores/admin/tabs";
-import {onUpdated, ref} from "vue";
-import {useAdminHomeStore} from "../../stores/admin/home";
-import {useRouter} from "vue-router";
+import { type Tab, useTabsStore } from '../..//stores/admin/tabs'
+import { onUpdated, ref } from 'vue'
+import { useAdminHomeStore } from '../../stores/admin/home'
+import { useRouter } from 'vue-router'
 
-const tabsStore = useTabsStore();
-const homeStore = useAdminHomeStore();
+const tabsStore = useTabsStore()
+const homeStore = useAdminHomeStore()
 
-const homeTab = ref();
-const settingsTab = ref();
-const router = useRouter();
+const homeTab = ref()
+const settingsTab = ref()
+const router = useRouter()
 
 // const openTabs = ref<Tab[] | null>();
 
-homeTab.value = tabsStore.getTabs.find((tab: Tab) => tab.name === "Home");
-settingsTab.value = tabsStore.getTabs.find((tab: Tab) => tab.name === "Settings");
-
+homeTab.value = tabsStore.getTabs.find((tab: Tab) => tab.name === 'Home')
+settingsTab.value = tabsStore.getTabs.find((tab: Tab) => tab.name === 'Settings')
 
 onUpdated(() => {
-  homeTab.value = tabsStore.getOpenTabs.find((tab: Tab) => tab.name === "Home");
-  settingsTab.value = tabsStore.getOpenTabs.find((tab: Tab) => tab.name === "Settings");
-});
+  homeTab.value = tabsStore.getOpenTabs.find((tab: Tab) => tab.name === 'Home')
+  settingsTab.value = tabsStore.getOpenTabs.find((tab: Tab) => tab.name === 'Settings')
+})
 
 const openModal = () => {
-  homeStore.openCreateDialog();
+  homeStore.openCreateDialog()
+  router.push({
+    name: 'CreateBot'
+  })
+  // console.log('clicked')
 }
 
 const openSearchModal = () => {
-  homeStore.openSearchDialog();
+  homeStore.openSearchDialog()
 }
 
+const onOpenProductDialog = () => {
+  homeStore.openProductDialog()
+  // console.log('Products clicked')
+}
 
 const onSettingsClick = () => {
   // navigate to the tab
@@ -37,22 +44,27 @@ const onSettingsClick = () => {
   setTimeout(async () => {
     try {
       // activate the tab
-      await tabsStore.updateActiveTab("Settings");
+      await tabsStore.updateActiveTab('Settings')
 
-      await  router.replace({name: "AdminSettingsTab", params: {tab: "General"}, query: {pageId: settingsTab.value?.pageId}});
+      await router.replace({
+        name: 'AdminSettingsTab',
+        params: { tab: 'General' },
+        query: { pageId: settingsTab.value?.pageId }
+      })
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
-  }, 300);
-};
+  }, 300)
+}
 
-const activeTab = tabsStore.getActiveTab;
+const activeTab = tabsStore.getActiveTab
 </script>
+
 
 <template>
   <header class="z-30 flex flex-row w-full overflow-hidden p-2 border-b">
     <div class="h-10 sm:h-14 p-2 sm:p-3">
-      <img src="/icon.png" class="w-full h-full object-center" alt="Bot Icon Img">
+      <img src="/icon.png" class="w-full h-full object-center" alt="Bot Icon Img" />
     </div>
 
     <span class="self-center material-icons-round text-2xl text-neutral-400">
@@ -61,70 +73,79 @@ const activeTab = tabsStore.getActiveTab;
 
     <div class="flex flex-row items-center w-full justify-between px-3">
       <div class="animate-slide-right flex items-center justify-center">
-        <span class="self-center text-2xl font-poppins-bold whitespace-nowrap dark:text-white">{{ activeTab?.name }}</span>
+        <span class="self-center text-2xl font-poppins-bold whitespace-nowrap dark:text-white">{{
+          activeTab?.name
+        }}</span>
       </div>
 
       <div class="flex flex-row">
         <div class="flex flex-row items-center justify-center">
-          <button class="btn btn-sm btn-ghost normal-case rounded-full md:rounded-md font-medium"
-                  @click="openSearchModal">
+          <button
+            class="btn btn-sm btn-ghost normal-case rounded-full md:rounded-md font-medium"
+            @click="openSearchModal"
+          >
             <i class="material-icons text-neutral-400 !text-2xl">search</i>
-            <span class="hidden md:inline-block">
-              Search
-            </span>
+            <span class="hidden md:inline-block"> Search </span>
           </button>
-          <button class="btn btn-sm btn-ghost rounded-full md:rounded-md normal-case font-medium"
-                  @click="openModal">
+          <button
+            class="btn btn-sm btn-ghost rounded-full md:rounded-md normal-case font-medium"
+            @click="openModal"
+          >
             <i class="material-icons text-neutral-400 !text-2xl">add_box</i>
-            <span class="hidden md:inline-block">
-            New Bot
-            </span>
+            <span class="hidden md:inline-block"> New Bot </span>
           </button>
-          <button class="btn btn-sm btn-ghost rounded-full md:rounded-md normal-case font-medium"
-                  @click="onSettingsClick">
+          <button
+            class="btn btn-sm btn-ghost rounded-full md:rounded-md normal-case font-medium"
+            @click="onSettingsClick"
+          >
             <i class="material-icons text-neutral-400 !text-2xl">settings</i>
-            <span class="hidden md:inline-block">
-            Settings
-            </span>
+            <span class="hidden md:inline-block"> Settings </span>
+          </button>
+          <button
+            class="btn btn-sm btn-ghost rounded-full md:rounded-md normal-case font-medium"
+            @click="onOpenProductDialog"
+
+          >
+            <span class="material-icons-outlined text-neutral-400">inventory_2</span>
+<!--            <i class="material-icons text-neutral-400 !text-2xl">settings</i>-->
+            <span class="hidden md:inline-block"> Products </span>
           </button>
         </div>
       </div>
     </div>
   </header>
-<!--  <div class="z-30 bg-neutral-100 flex flex-row w-full overflow-hidden">-->
-<!--    <div class="border-b p-2"></div>-->
-<!--    <div class="px-3 py-2 flex flex-row items-center justify-start bg-neutral-100 border-y">-->
-<!--      <h6 class="text-base font-semibold">-->
-<!--        M-Zawadi-->
-<!--      </h6>-->
-<!--    </div>-->
-<!--    &lt;!&ndash; Let's have the first default tab &ndash;&gt;-->
-<!--    &lt;!&ndash;    <BarItem title="Home" icon="home" to="home" name="Home" active/>&ndash;&gt;-->
-<!--    &lt;!&ndash; Let's loop through the opened tabs &ndash;&gt;-->
-<!--    <BarItem v-if="homeTab" :active="homeTab.active" :icon="homeTab.icon" :name="homeTab.name"-->
-<!--             :page-id="homeTab.pageId"-->
-<!--             :title="homeTab.title" :to="homeTab.to"/>-->
-<!--    <div class="flex flex-row w-8/12 overflow-auto">-->
-<!--      <template v-for="(tab) in tabsStore.getOpenTabs">-->
-<!--        <BarItem v-if="tab.name !== 'Settings' && tab.name !== 'Home'" :key="tab.name" :active="tab.active"-->
-<!--                 :name="tab.name" :page-id="tab.pageId"-->
-<!--                 :title="tab.title" :to="tab.to" icon="home"/>-->
-<!--      </template>-->
-<!--    </div>-->
-<!--    <div class="flex flex-row items-center justify-start bg-neutral-100 grow border-y border-l">-->
-<!--      <div class="p-3 flex items-center justify-center">-->
-<!--        <button class="btn btn-xs btn-square btn-ghost" @click="openModal">-->
-<!--          <i class="material-icons text-neutral-400 !text-base">add</i>-->
-<!--        </button>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--    <BarItem :active="settingsTab.active" :icon="settingsTab.icon" :name="settingsTab.name"-->
-<!--             :page-id="settingsTab.pageId"-->
-<!--             :title="settingsTab.title" :to="settingsTab.to"/>-->
-<!--    <div class="border-l border-b p-2"></div>-->
-<!--  </div>-->
+  <!--  <div class="z-30 bg-neutral-100 flex flex-row w-full overflow-hidden">-->
+  <!--    <div class="border-b p-2"></div>-->
+  <!--    <div class="px-3 py-2 flex flex-row items-center justify-start bg-neutral-100 border-y">-->
+  <!--      <h6 class="text-base font-semibold">-->
+  <!--        M-Zawadi-->
+  <!--      </h6>-->
+  <!--    </div>-->
+  <!--    &lt;!&ndash; Let's have the first default tab &ndash;&gt;-->
+  <!--    &lt;!&ndash;    <BarItem title="Home" icon="home" to="home" name="Home" active/>&ndash;&gt;-->
+  <!--    &lt;!&ndash; Let's loop through the opened tabs &ndash;&gt;-->
+  <!--    <BarItem v-if="homeTab" :active="homeTab.active" :icon="homeTab.icon" :name="homeTab.name"-->
+  <!--             :page-id="homeTab.pageId"-->
+  <!--             :title="homeTab.title" :to="homeTab.to"/>-->
+  <!--    <div class="flex flex-row w-8/12 overflow-auto">-->
+  <!--      <template v-for="(tab) in tabsStore.getOpenTabs">-->
+  <!--        <BarItem v-if="tab.name !== 'Settings' && tab.name !== 'Home'" :key="tab.name" :active="tab.active"-->
+  <!--                 :name="tab.name" :page-id="tab.pageId"-->
+  <!--                 :title="tab.title" :to="tab.to" icon="home"/>-->
+  <!--      </template>-->
+  <!--    </div>-->
+  <!--    <div class="flex flex-row items-center justify-start bg-neutral-100 grow border-y border-l">-->
+  <!--      <div class="p-3 flex items-center justify-center">-->
+  <!--        <button class="btn btn-xs btn-square btn-ghost" @click="openModal">-->
+  <!--          <i class="material-icons text-neutral-400 !text-base">add</i>-->
+  <!--        </button>-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <BarItem :active="settingsTab.active" :icon="settingsTab.icon" :name="settingsTab.name"-->
+  <!--             :page-id="settingsTab.pageId"-->
+  <!--             :title="settingsTab.title" :to="settingsTab.to"/>-->
+  <!--    <div class="border-l border-b p-2"></div>-->
+  <!--  </div>-->
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
