b26790e803619eb97cf7c522d5b565425f24ddff
muhonja <linahmuhonjaimbari>
2024-05-29 10:05:24 +0300

    add chatbot name Validations

--- src/views/HomeAdmin.vue
+++ src/views/HomeAdmin.vue
@@ -1,149 +1,83 @@
 <script lang="ts" setup>
-import TopBar from "../components/Admin/TopBar.vue";
-import {useAdminHomeStore} from "../stores/admin/home";
-import DialogModal from "../components/DialogModal.vue";
-import {useRouter} from "vue-router";
-import {useTabsStore} from "../stores/admin/tabs";
-import {usePageContentStore} from "../stores/admin/page-data";
-import {useField} from "vee-validate";
-import ToastContainer from "../views/Admin/toasts/ToastContainer.vue";
-import ToastAlert from "../views/Admin/toasts/ToastAlert.vue";
-import {useNotificationsStore} from "../stores/notifications";
-import PagesSidebar from "./Admin/PagesSidebar.vue";
-import {computed, ref } from "vue";
-import PageSearchItem from "./Admin/PageSearchItem.vue";
-import {gsap} from "gsap";
+import TopBar from '../components/Admin/TopBar.vue'
+import { useAdminHomeStore } from '../stores/admin/home'
+import DialogModal from '../components/DialogModal.vue'
+import { useRouter } from 'vue-router'
 
-const router = useRouter();
-const tabsStore = useTabsStore();
-const pageContentStore = usePageContentStore();
-const homeStore = useAdminHomeStore();
-const notificationsStore = useNotificationsStore();
+import ToastContainer from '../views/Admin/toasts/ToastContainer.vue'
+import ToastAlert from '../views/Admin/toasts/ToastAlert.vue'
+import { useNotificationsStore } from '../stores/notifications'
+import PagesSidebar from './Admin/PagesSidebar.vue'
+import {computed, reactive, type Ref, ref, watch} from 'vue'
+import PageSearchItem from './Admin/PageSearchItem.vue'
+import { gsap } from 'gsap'
 
-// by grace before we get here, the pages have already been fetched
-
-const allPageNames = homeStore.getPages.map((page) => page.name.toLowerCase());
-
-const pageNameValidator = (value: string) => {
-  if (!value) {
-    return "Page name is required";
-  }
-
-  if (value.length < 3) {
-    return "Page name must be at least 3 characters long";
-  }
-
-  if (value.length > 50) {
-    return "Page name must not exceed 50 characters";
-  }
-
-  // allow only alphanumeric characters
-  const pageNameRegex = /^[a-zA-Z0-9]+$/;
-
-  if (!pageNameRegex.test(value)) {
-    return "Page name must only contain alphanumeric characters";
-  }
-
-  if (allPageNames.includes(value.toLowerCase())) {
-    return "Page name already exists";
-  }
-
-  if (value.toLowerCase() === "home" || value.toLowerCase() === "settings") {
-    return "Page name is reserved";
-  }
-
-  return true;
-}
-
-const {
-  value: pageName,
-  errorMessage: pageNameErrorMessage,
-  meta: pageNameMeta,
-} = useField("pageName", pageNameValidator);
-
-const closeCreateModal = () => {
-  homeStore.closeCreateDialog();
-}
+const router = useRouter()
+const homeStore = useAdminHomeStore()
+const notificationsStore = useNotificationsStore()
 
 const closeDeleteModal = () => {
-  homeStore.closeDeleteDialog();
+  homeStore.closeDeleteDialog()
 }
 
 const closeSearchModal = () => {
-  homeStore.closeSearchDialog();
+  homeStore.closeSearchDialog()
 }
 
-const createPage = async () => {
-  if (pageNameMeta.valid) {
-
-    const page = await homeStore.createNewPage(pageName.value) as any;
-
-    console.log(page);
-
-    if (page) {
-
-      tabsStore.setActiveTabByPageName(page.name);
-
-      pageContentStore.setActivePageContentItem(page.name);
-
-      homeStore.closeCreateDialog();
-
-      // reset the page name field
-      pageName.value = "";
-      pageNameMeta.valid = false;
-      pageNameMeta.validated = false;
-      pageNameMeta.touched = false;
-
-      await router.push({name: "DynamicPage", params: {page: page.pageSlug}, query: {pageId: page.pageId}});
-    }
-  }
-}
 
 const deletePage = async () => {
-  const page = await homeStore.deletePage() as any;
+  const page = (await homeStore.deletePage()) as any
 
   if (page) {
-    homeStore.closeDeleteDialog();
+    homeStore.closeDeleteDialog()
 
-    await router.push({name: "AdminHome", params: {page: "home"}, query: {pageId: page.pageId}});
+    await router.push({
+      name: 'AdminHome',
+      params: { page: 'home' },
+      query: { pageId: page.pageId }
+    })
   }
 }
 
 // let's implement searching for the pages
 
-const pagesToDisplay = ref<{   id: string,  name: string,  path: string,  title: string} []>([]);
+const pagesToDisplay = ref<{ id: string; name: string; path: string; title: string }[]>([])
 
 if (homeStore.getPages.length > 0) {
-  pagesToDisplay.value = homeStore.getPages.slice(0, 5);
+  pagesToDisplay.value = homeStore.getPages.slice(0, 5)
 } else {
-  pagesToDisplay.value = [];
+  pagesToDisplay.value = []
 }
 
-const searchQueryText = ref("");
+
+
+const searchQueryText = ref('')
 
 const searchPages = (e: Event) => {
-  hasInput.value = true;
+  hasInput.value = true
 
-  const target = e.target as HTMLInputElement;
+  const target = e.target as HTMLInputElement
 
-  const searchQuery = target.value;
+  const searchQuery = target.value
 
-  searchQueryText.value = searchQuery;
+  searchQueryText.value = searchQuery
 
   if (searchQuery.length > 0) {
-    pagesToDisplay.value = homeStore.getPages.filter((page) => {
-      return page.name.toLowerCase().includes(searchQuery.toLowerCase());
-    }).slice(0, 5);
+    pagesToDisplay.value = homeStore.getPages
+      .filter((page) => {
+        return page.name.toLowerCase().includes(searchQuery.toLowerCase())
+      })
+      .slice(0, 5)
   } else {
-    pagesToDisplay.value = homeStore.getPages.slice(0, 5);
+    pagesToDisplay.value = homeStore.getPages.slice(0, 5)
   }
 }
 
-const hasInput = ref(false);
+const hasInput = ref(false)
 
 const pagesToDisplayLength = computed(() => {
-  return pagesToDisplay.value.length;
-});
+  return pagesToDisplay.value.length
+})
 
 function onEnter(el: any, done: any) {
   gsap.to(el, {
@@ -166,122 +100,77 @@ function onLeave(el: any, done: any) {
 </script>
 
 <template>
-
   <div class="flex flex-col min-h-screen">
     <!-- Page Heading -->
-    <TopBar/>
+    <TopBar />
     <!-- End Page Heading -->
-    <div class="bg-gray-50 dark:bg-slate-900 relative flex-1 grow">
+    <div class="bg-gray-50 dark:bg-slate-900 relative flex-1 grow flex min-h-full">
       <!-- ========== MAIN CONTENT ========== -->
 
-      <PagesSidebar/>
+      <PagesSidebar />
 
-      <div class="md:ml-64 flex flex-col flex-1 grow">
+      <div class="md:ml-64 flex flex-col flex-1 grow min-h-full">
         <RouterView #default="{ Component, route }">
           <Transition name="route" mode="out-in" appear>
             <template v-if="Component">
-              <component :is="Component" :key="route.fullPath"/>
+              <component :is="Component" :key="route.fullPath" />
             </template>
           </Transition>
         </RouterView>
       </div>
-
     </div>
 
     <Teleport to="body">
-      <DialogModal :is-open="homeStore.createDialog.isOpen" @closeModal="closeCreateModal">
-        <template #title>
-          <span class="text-lg font-semibold">
-            Create a new bot
-          </span>
-        </template>
-
-        <template #body>
-          <form class="grid grid-cols-1 gap-3" @submit.prevent="createPage">
-            <div class="flex flex-col space-y-2">
-              <label class="label text-xs font-semibold" for="chatbot-name">
-                Bot Name
-              </label>
-              <input
-                  id="page-name"
-                  v-model="pageName"
-                  :class="{'input-error': pageNameMeta.validated && !pageNameMeta.valid, 'input-primary': pageNameMeta.validated && pageNameMeta.valid}"
-                  class="input input-bordered input-primary w-full text-sm" placeholder="Bot Name"
-                  type="text"
-              />
-              <small v-if="pageNameMeta.validated && !pageNameMeta.valid"
-                     class="text-xs text-rose-500">
-                {{ pageNameErrorMessage }}
-              </small>
-              <small class="text-xs text-gray-500">
-                This will be used as the bot's name across the application. More configurations will be shown after creating the bot.
-              </small>
-            </div>
-          </form>
-        </template>
-
-        <template #footer>
-          <div class="flex flex-row items-center space-x-2 w-full">
-            <button class="grow btn btn-sm md:btn-md btn-ghost normal-case border border-1 border-gray-400"
-                    @click="closeCreateModal">
-              Cancel
-            </button>
-            <button :disabled="!homeStore.enabledCreateDialogBtn"
-                    class="grow btn btn-sm md:btn-md btn-primary normal-case"
-                    @click="createPage">
-              Create
-            </button>
-          </div>
-        </template>
-      </DialogModal>
-
       <DialogModal :is-open="homeStore.deleteDialog.isOpen" @closeModal="closeDeleteModal">
         <template #title>
-          <span class="text-lg font-semibold">
-            Delete bot
-          </span>
+          <span class="text-lg font-semibold"> Delete bot </span>
         </template>
 
         <template #body>
           <div class="grid grid-cols-1 gap-3">
             <div class="flex flex-col space-y-2">
-              <p class="text-sm">
-                Are you sure you want to delete this page?
-              </p>
+              <p class="text-sm">Are you sure you want to delete this page?</p>
             </div>
           </div>
         </template>
 
         <template #footer>
           <div class="flex flex-row items-center space-x-2 w-full">
-            <button class="grow btn btn-sm md:btn-md btn-ghost normal-case border border-1 border-gray-400"
-                    @click="closeDeleteModal">
+            <button
+              class="grow btn btn-sm md:btn-md btn-ghost normal-case border border-1 border-gray-400"
+              @click="closeDeleteModal"
+            >
               Cancel
             </button>
-            <button :disabled="!homeStore.enabledDeleteDialogBtn"
-                    class="grow btn btn-sm md:btn-md btn-error normal-case text-white"
-                    @click="deletePage">
+            <button
+              :disabled="!homeStore.enabledDeleteDialogBtn"
+              class="grow btn btn-sm md:btn-md btn-error normal-case text-white"
+              @click="deletePage"
+            >
               Delete
             </button>
           </div>
         </template>
       </DialogModal>
 
-      <DialogModal :is-open="homeStore.searchDialog.isOpen" @closeModal="closeSearchModal" max-width="w-9/12 sm:w-6/12 md:w-5/12">
+      <DialogModal
+        :is-open="homeStore.searchDialog.isOpen"
+        @closeModal="closeSearchModal"
+        max-width="w-9/12 sm:w-6/12 md:w-5/12"
+      >
         <template #title>
-          <span class="text-lg font-semibold">
-            Search bots
-          </span>
+          <span class="text-lg font-semibold"> Search bots </span>
         </template>
 
         <template #body>
           <div class="grid grid-cols-1 gap-3 mt-3">
             <div class="flex flex-col space-y-2">
               <input
-                  id="search-pages"
-                  @input="searchPages"
-                  class="input input-bordered input-primary w-full text-sm" placeholder="Search bots"
-                  type="text"
+                id="search-pages"
+                @input="searchPages"
+                class="input input-bordered input-primary w-full text-sm"
+                placeholder="Search bots"
+                type="text"
               />
             </div>
             <div class="flex flex-col space-y-2">
@@ -292,7 +181,9 @@ function onLeave(el: any, done: any) {
 
                 <div class="flex flex-col space-y-2 pt-4">
                   <div class="flex flex-row justify-center items-center">
-                    <div class="bg-neutral-100 rounded-full w-12 h-12 flex items-center justify-center">
+                    <div
+                      class="bg-neutral-100 rounded-full w-12 h-12 flex items-center justify-center"
+                    >
                       <div class="material-icons-round !text-3xl text-neutral-500">
                         <span v-if="!hasInput">search</span>
                         <span v-else>search_off</span>
@@ -308,16 +199,25 @@ function onLeave(el: any, done: any) {
                       No results found for "{{ searchQueryText }}"
                     </p>
 
-                    <p class="text-sm text-gray-400">
-                      Try searching for something else
-                    </p>
+                    <p class="text-sm text-gray-400">Try searching for something else</p>
                   </div>
                 </div>
               </div>
               <div v-else class="flex flex-col space-y-2">
-                <TransitionGroup :css="false" @enter="onEnter" @leave="onLeave" name="fade" tag="div" class="flex flex-col space-y-2">
-                  <PageSearchItem v-for="(page, index) in pagesToDisplay" :key="page.id" :page="page"
-                                  :data-index="index"/>
+                <TransitionGroup
+                  :css="false"
+                  @enter="onEnter"
+                  @leave="onLeave"
+                  name="fade"
+                  tag="div"
+                  class="flex flex-col space-y-2"
+                >
+                  <PageSearchItem
+                    v-for="(page, index) in pagesToDisplay"
+                    :key="page.id"
+                    :page="page"
+                    :data-index="index"
+                  />
                 </TransitionGroup>
               </div>
             </div>
@@ -327,10 +227,17 @@ function onLeave(el: any, done: any) {
 
       <ToastContainer v-if="notificationsStore.hasNotifications">
         <!--        <ToastAlert text="Page name is required" type="error" id=""/>-->
-        <template v-for="(notification) in notificationsStore.getNotifications" :key="notification.id">
-        <ToastAlert v-if="notification.id && notification.isShown" :id ="notification.id"
-                    :is-shown="notification.isShown" :message="notification.message"
-                    :type="notification.type"/>
+        <template
+          v-for="notification in notificationsStore.getNotifications"
+          :key="notification.id"
+        >
+          <ToastAlert
+            v-if="notification.id && notification.isShown"
+            :id="notification.id"
+            :is-shown="notification.isShown"
+            :message="notification.message"
+            :type="notification.type"
+          />
         </template>
       </ToastContainer>
     </Teleport>
@@ -338,19 +245,18 @@ function onLeave(el: any, done: any) {
 </template>
 
 <style scoped>
-
 .route-enter-from {
   opacity: 0;
-  transform: translateY(50%)
+  transform: translateY(50%);
 }
 
 .route-enter-active,
 .route-leave-active {
-  transition: all .35s ease-out
+  transition: all 0.35s ease-out;
 }
 
 .route-leave-to {
   opacity: 0;
-  transform: translateY(50%)
+  transform: translateY(50%);
 }
 </style>
