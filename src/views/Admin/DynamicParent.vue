7f7e33a329e786444acafa6dabe3ccbde9b60cd3
muhonja <linahmuhonjaimbari>
2024-05-29 09:59:45 +0300

    add parent component with RouterView

---
+++ src/views/Admin/DynamicParent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <RouterView #default="{Component, route}">
+    <transition>
+<!--      <template v-if="Component">-->
+        <component :is="Component" :key="route.fullPath">
+        </component>
+<!--      </template>-->
+    </transition>
+
+  </RouterView>
+</template>
