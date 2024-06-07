9dc7b8b5fabbdd7adeddfdc56ef2cd594840f07d
muhonja <linahmuhonjaimbari@gmail.com>
2024-06-07 23:13:27 +0300

    add admin Login Page

--- src/views/Auth/AdminLoginPage.vue
+++ src/views/Auth/AdminLoginPage.vue
@@ -1,49 +1,53 @@
-<script setup lang="ts">
-import {useField} from "vee-validate";
-import {useRouter} from "vue-router";
-import {reactive, watch, ref} from "vue";
-import {useAuthStore} from "../../stores/auth";
+<script lang="ts" setup>
+import { useField } from 'vee-validate'
+import { useRouter } from 'vue-router'
+import { reactive, ref, watch } from 'vue'
+import { useAuthStore } from '../../stores/auth'
+import { useNotificationsStore } from '@/stores/notifications'
 
-const router = useRouter();
-const authStore = useAuthStore();
+const router = useRouter()
+const authStore = useAuthStore()
+const notificationStore = useNotificationsStore()
 
 const loginData = reactive({
   email: '',
-  password: '',
-});
+  password: ''
+})
 
 const emailValidator = (value: string) => {
   if (!value) {
-    return 'Email is required';
+    return 'Email is required'
   }
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
   if (!emailRegex.test(value)) {
-    return 'Email must be valid';
+    return 'Email must be valid'
   }
 
   if (value.length > 50) {
-    return 'Email must be less than 50 characters';
+    return 'Email must be less than 50 characters'
   }
 
-  return true;
-};
+  return true
+}
 
 const {
   value: email,
   errorMessage: emailErrorMessage,
-  meta: emailMeta,
-} = useField('email', emailValidator);
+  meta: emailMeta
+} = useField('email', emailValidator)
 
-watch(() => loginData.email, (value) => {
-  email.value = value;
-});
-
+watch(
+  () => loginData.email,
+  (value) => {
+    email.value = value
+  }
+)
 
 const passwordValidator = (value: string) => {
   if (!value) {
-    return 'Password is required';
+    return 'Password is required'
   }
 
   // if (value.length < 4) {
@@ -51,101 +55,124 @@ const passwordValidator = (value: string) => {
   // }
 
   if (value.length > 50) {
-    return 'Password must be less than 50 characters';
+    return 'Password must be less than 50 characters'
   }
 
-  return true;
-};
+  return true
+}
 
 const {
   value: password,
   errorMessage: passwordErrorMessage,
-  meta: passwordMeta,
-} = useField('password', passwordValidator);
+  meta: passwordMeta
+} = useField('password', passwordValidator)
 
-watch(() => loginData.password, (value) => {
-  password.value = value;
-});
+watch(
+  () => loginData.password,
+  (value) => {
+    password.value = value
+  }
+)
 
-const isLoadingResource = ref(false);
-const loginFailed = ref(false);
+const isLoadingResource = ref(false)
+const loginFailed = ref(false)
 
 const onLoginClick = () => {
   if (!emailMeta.valid || !passwordMeta.valid) {
-    return;
+    return
   }
 
-  loginFailed.value = false;
-  isLoadingResource.value = true;
+  loginFailed.value = false
+  isLoadingResource.value = true
 
-
-  authStore.adminLogin(loginData)
-      .then((response) => {
-        isLoadingResource.value = false;
-        console.log(response);
-        router.push({name: 'AdminHome'});
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-};
-
+  authStore
+    .adminLogin(loginData)
+    .then((response) => {
+      isLoadingResource.value = false
+      console.log(response)
+      if (response.success) {
+        notificationStore.addNotification('You are successfully logged in', 'success')
+        router.push({ name: 'AdminHome' })
+      } else {
+        notificationStore.addNotification('Login failed', 'error')
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
 </script>
 
 <template>
   <main class="w-full mx-auto p-6 flex items-center justify-center h-screen">
     <div
-        class="w-full md:w-6/12 lg:w-5/12 xl:w-4/12 px-4 md:px-2 lg:px-0 mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+      class="w-full md:w-6/12 lg:w-5/12 xl:w-4/12 px-4 md:px-2 lg:px-0 mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700"
+    >
       <div class="p-4 sm:p-7">
         <div class="text-center">
           <h1 class="block text-2xl font-bold text-gray-800 dark:text-white">Login</h1>
         </div>
 
         <div class="mt-5">
-          <form @submit.prevent="onLoginClick" class="my-4">
+          <form class="my-4" @submit.prevent="onLoginClick">
             <div class="grid gap-y-4">
               <div class="flex flex-col space-y-1">
                 <div class="flex justify-between items-center">
-                  <label class="label font-semibold text-sm" for="email">
-                    Email
-                  </label>
-<!--                  <router-link-->
-<!--                      class="text-sm text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"-->
-<!--                      to="forgot-password">Forgot password?-->
-<!--                  </router-link>-->
+                  <label class="label font-semibold text-sm" for="email"> Email </label>
+                  <!--                  <router-link-->
+                  <!--                      class="text-sm text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"-->
+                  <!--                      to="forgot-password">Forgot password?-->
+                  <!--                  </router-link>-->
                 </div>
                 <input
-                    id="email"
-                    v-model="loginData.email"
-                    :class="{'input-error': (emailMeta.validated && !emailMeta.valid), 'input-primary': emailMeta.validated && emailMeta.valid}"
-                    class="input input-primary input-bordered w-full text-sm" placeholder="Email" type="text"/>
-                <small v-if="(emailMeta.validated && !emailMeta.valid)"
-                       class="text-sm text-rose-500">
+                  id="email"
+                  v-model="loginData.email"
+                  :class="{
+                    'input-error': emailMeta.validated && !emailMeta.valid,
+                    'input-primary': emailMeta.validated && emailMeta.valid
+                  }"
+                  class="input input-primary input-bordered w-full text-sm"
+                  placeholder="Email"
+                  type="text"
+                />
+                <small v-if="emailMeta.validated && !emailMeta.valid" class="text-sm text-rose-500">
                   {{ emailErrorMessage }}
                 </small>
               </div>
 
               <div class="flex flex-col space-y-1">
-                <label class="label font-semibold text-sm" for="password">
-                  Password
-                </label>
+                <label class="label font-semibold text-sm" for="password"> Password </label>
                 <input
-                    id="password"
-                    v-model="loginData.password"
-                    :class="{'input-error': (passwordMeta.validated && !passwordMeta.valid), 'input-primary': passwordMeta.validated && passwordMeta.valid}"
-                    class="input input-primary input-bordered w-full text-sm" placeholder="Password" type="password"/>
-                <small v-if="(passwordMeta.validated && !passwordMeta.valid)"
-                       class="text-sm text-rose-500">
+                  id="password"
+                  v-model="loginData.password"
+                  :class="{
+                    'input-error': passwordMeta.validated && !passwordMeta.valid,
+                    'input-primary': passwordMeta.validated && passwordMeta.valid
+                  }"
+                  class="input input-primary input-bordered w-full text-sm"
+                  placeholder="Password"
+                  type="password"
+                />
+                <small
+                  v-if="passwordMeta.validated && !passwordMeta.valid"
+                  class="text-sm text-rose-500"
+                >
                   {{ passwordErrorMessage }}
                 </small>
               </div>
 
               <div class="flex flex-col space-y-1 my-1">
-                <button @click="onLoginClick" :disabled="isLoadingResource"
-                        type="submit" class="btn btn-primary btn-sm md:btn-md normal-case text-xs md:text-sm w-full">
-                  <span v-if="isLoadingResource" class="loading loading-md loading-spinner text-neutral-400"></span>
-                  Sign
-                  in
+                <button
+                  :disabled="isLoadingResource"
+                  class="btn btn-primary btn-sm md:btn-md normal-case text-xs md:text-sm w-full"
+                  type="submit"
+                  @click="onLoginClick"
+                >
+                  <span
+                    v-if="isLoadingResource"
+                    class="loading loading-md loading-spinner text-neutral-400"
+                  ></span>
+                  Sign in
                 </button>
               </div>
             </div>
@@ -157,6 +184,4 @@ const onLoginClick = () => {
   </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
