8e26d608ab7d08b73f571d200512e376470bc803
muhonja <linahmuhonjaimbari@gmail.com>
2024-06-08 12:45:28 +0300

    add ConversationHistory Page

---
+++ src/views/chatbot/ConversationHistoryPage.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+
+interface ConversationHistoryPageProps {
+  // all chatbot names are lowercase
+  chatbotName: string
+}
+
+const props = withDefaults(defineProps<ConversationHistoryPageProps>(), {
+  chatbotName: 'Mzawadi'
+})
+
+onMounted(() => {
+  console.log('ConversationHistoryPage mounted')
+  console.log('Chatbot Name:', props.chatbotName)
+})
+</script>
+
+<template>
+  <div>
+    <h1>Conversation History Page</h1>
+    <p>Chatbot Name: {{ props.chatbotName }}</p>
+  </div>
+</template>
+
+<style scoped></style>
