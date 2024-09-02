c46df788d45264690439cae2afa43b15e635d750
muhonja <linahmuhonjaimbari@gmail.com>
2024-09-02 10:16:31 +0300

    add text color to the history headings

--- src/views/chatbot/ConversationHistoryPage.vue
+++ src/views/chatbot/ConversationHistoryPage.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-import { useChatbotStore } from '@/stores/chatbot'
-import { useNotificationsStore } from '@/stores/notifications'
-import { useRouter } from 'vue-router'
+import {useChatbotStore} from '@/stores/chatbot'
+import {useNotificationsStore} from '@/stores/notifications'
+import {useRouter} from 'vue-router'
 
 const chatBotStore = useChatbotStore()
 const router = useRouter()
@@ -12,11 +12,13 @@ interface ConversationHistoryPageProps {
   conversationId: string
   createdAt: string
   title: string
+  botBubbleColor: string
 }
 
 const notification = useNotificationsStore()
 
 const props = defineProps<ConversationHistoryPageProps>()
+console.log("*************", props)
 const viewHistory = (convId: string) => {
   chatBotStore.setNewChatButton(false)
   chatBotStore.displayHistory(convId).then((response) => {
@@ -26,7 +28,7 @@ const viewHistory = (convId: string) => {
       console.log('chatDisplay', chatBotStore.chatDisplayArray)
       router.push({
         name: 'chatHistory',
-        params: { convId: convId, cbName: chatBotStore.pgSlug }
+        params: {convId: convId, cbName: chatBotStore.pgSlug}
       })
     } else {
       notification.addNotification('error occurred', 'error')
@@ -38,13 +40,17 @@ const viewHistory = (convId: string) => {
 <template>
   <div class="w-52 flex flex-row text-sm pb-2 overflow-hidden">
     <button
-      id="histButton"
-      :class="{ 'bg-emerald-100': chatBotStore.getActiveHistory === props.id }"
-      class="btn btn-ghost btn-sm"
-      @click="viewHistory(props.conversationId)"
+        id="histButton"
+        :class="[chatBotStore.getActiveHistory === props.id? props.botBubbleColor: '']"
+        class="btn btn-ghost btn-sm"
+        @click="viewHistory(props.conversationId)"
     >
-      <span class="material-icons-outlined !text-sm text-neutral-600"> chat_bubble_outline </span>
-      <span class="text-neutral-600">{{ props.title }}</span>
+      <span
+          class="material-icons-outlined !text-sm text-neutral-600"> chat_bubble_outline </span>
+      <span
+          class="text-neutral-600">{{
+          props.title
+        }}</span>
     </button>
   </div>
 </template>
